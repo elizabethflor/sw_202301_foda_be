7f6104f2029b1b83da046f8e304385c8c8fb28cb
Orlando J Betancourth <orlando.betancourth@gmail.com>
2023-02-27 18:55:29 -0600

    Foda Dao

--- src/libs/FODA/Foda.ts
+++ src/libs/FODA/Foda.ts
@@ -3,11 +3,10 @@ import { IEmpresa } from "@dao/models/Empresas/IEmpresas";
 import { IDataAccessObject } from "@dao/IDataAccessObject";
 
 export class Foda{
-    private fodaDao: IDataAccessObject;
-    private empresaDao: IDataAccessObject;
-
-    constructor(foda:IDataAccessObject, empresa:IDataAccessObject){
-        this.fodaDao = foda;
-        this.empresaDao = empresa;
-    }
+  private fodaDao: IDataAccessObject;
+  private empresaDao: IDataAccessObject;
+  constructor(foda:IDataAccessObject, empresa: IDataAccessObject){
+    this.fodaDao = foda;
+    this.empresaDao = empresa;
+  }
 }
